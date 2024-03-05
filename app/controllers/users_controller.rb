e0d466f98311d837fbf3e837a11ea88e88cc414d
Jason Johnson <145226961+JJohnson606@users.noreply.github.com>
2024-03-05 20:51:26 +0000

    Fixed user likes association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def liked_photos
+    @user = User.find_by!(username: params.fetch(:username))
+    @liked_photos = @user.liked_photos
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,14 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos 
-  get "/:username" => "users#show", as: :user
 
+  
+  # get ":username/feed"
+  # get ":username/followers"
+  # get ":username/following"
+
+  get "/:username" => "users#show", as: :user
+  get ":username/liked" => "users#liked", as: :liked
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
